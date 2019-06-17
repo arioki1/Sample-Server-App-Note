becfe4b73e9c920ff4da4a0771c8a6e74a7db348
Yoga Setiawan <mryoga1995@gmail.com>
2019-06-17 16:44:09 +0700

    config server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 300;
+const bodyParser = require('body-parse');
+const routes = require('./routes');
+
+app.use(
+  bodyParser.urlencoded({
+    extended:true,
+  })
+);
+
+app.use(bodyParser.json());
+
+routes(app);
+
+app.listen(port);
+
+console.log('Server Started');
