01b63577a740b065e817d8e40b7a67e62986973b
Yoga Setiawan <mryoga1995@gmail.com>
2019-06-17 17:24:26 +0700

    fix port and import body-parser

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 300;
-const bodyParser = require('body-parse');
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 app.use(
