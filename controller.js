1ba5f3a1262e8dc4f460973c30b68c07c9626621
Yoga Setiawan <mryoga1995@gmail.com>
2019-06-17 17:25:46 +0700

    fix respon

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ const response = require('./response');
 const connection = require('./connect');
 
 exports.home = function (req, res) {
-    response.ok('Welcome to Server Sample Note App API');
+    response.ok('Welcome to Server Sample Note App API', res);
 };
