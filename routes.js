4aa6de395fc5032ded864294fe61b9d6be95858b
Yoga Setiawan <mryoga1995@gmail.com>
2019-06-18 10:15:09 +0700

    add middleware to write access logs

--- routes.js
+++ routes.js
@@ -4,15 +4,14 @@ module.exports = function (app) {
     const controller = require('./controller');
     //Route API Note
     app.route('/').get(controller.home);
-    app.route('/list_note').get(controller.list_note);
-    app.route('/insert_note').post(controller.insert_note);
-    app.route('/delete_note/:id').delete(controller.delete_category);
-    app.route('/update_note/:id').patch(controller.update_note);
+    app.route('/note/').get(controller.list_note);
+    app.route('/note').post(controller.insert_note);
+    app.route('/note/:id').delete(controller.delete_note);
+    app.route('/note/:id').patch(controller.update_note);
 
     //Route API Category
-    app.route('/list_category').get(controller.list_category);
-    app.route('/insert_category').post(controller.insert_category);
-    app.route('/delete_category/:id').delete(controller.delete_category);
-    app.route('/update_category/:id').patch(controller.update_category);
-
+    app.route('/category').get(controller.list_category);
+    app.route('/category').post(controller.insert_category);
+    app.route('/category/:id').delete(controller.delete_category);
+    app.route('/category/:id').patch(controller.update_category);
 };
