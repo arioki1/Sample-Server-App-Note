b331ca3e7bdae25feb75c56cb876aa08af32f930
Yoga Setiawan <mryoga1995@gmail.com>
2019-06-19 18:58:59 +0700

    fix sort data

--- controller.js
+++ controller.js
@@ -4,7 +4,6 @@ const response = require('./response');
 const connection = require('./connect');
 const dateFormat = require('dateformat');
 
-
 /*function getCountNote(callback)
 {
     connection.query('SELECT COUNT(*) as total FROM data_note', function(err, result)
@@ -51,7 +50,7 @@ exports.insertNote = function (req, res) {
     let note = req.body.note;
     let id_category = req.body.id_category;
     let title = req.body.title;
-    let time = dateFormat(new Date(), "yyyy-mm-dd h:MM:ss");
+    let time = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
 
     if (typeof note == 'undefined' || typeof id_category == 'undefined' || typeof title == 'undefined') {
         response.ok("Field note or id_category cannot null or empty", res);
@@ -114,22 +113,24 @@ exports.searchNote = function (req, res) {
     });
 };
 exports.sortNote = function (req, res) {
+    let sort = req.query.sort || "DESC";
+    let by = req.query.by || "time";
+
     const sql = `SELECT data_note.id, data_note.title, data_note.note, 
                 data_note.time, category_note.name as "name_category"
                 FROM data_note LEFT JOIN category_note 
                 ON data_note.id_category=category_note.id
-                ORDER BY data_note.title ASC`;
+                ORDER BY data_note.${by} ${sort}`;
     connection.query(sql, function (error, rows, field) {
         if (error) {
-            throw error;
+            response.ok("Note does not found", res);
         } else {
-            (rows.length > 0) ? response.ok(rows, res) : response.ok("Note does not exit", res);
+            (rows.length > 0) ? response.ok(rows, res) : response.ok("Note does not found", res);
         }
     });
 };
 exports.pageNote = function (req, res) {
     let page = req.query.page || 0;
-    ;
     let limit = 5;
     let countNote;
     let maxPage;
